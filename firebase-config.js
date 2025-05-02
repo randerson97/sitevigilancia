ae611063891936fccd9f4d5f6fe1e5232dd4e17d
randerson97 <133051210+randerson97@users.noreply.github.com>
2025-05-02 10:50:14 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
